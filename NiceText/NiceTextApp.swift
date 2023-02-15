68084c3561cf5cf27a3cafa6092affd86766e0c3
Nicholas Cory <nacory@iastate.edu>
2023-02-15 16:57:41 -0600

    Initial Commit

---
+++ NiceText/NiceTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceTextApp.swift
+//  NiceText
+//
+//  Created by Nick Cory on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NiceTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
