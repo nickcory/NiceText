b5969ae6f34e59edd876b1278c54df96aa76d20a
Nicholas Cory <nacory@iastate.edu>
2023-02-15 17:53:18 -0600

    Read and write plain text files on iOS and macOS.

--- NiceText/ContentView.swift
+++ NiceText/ContentView.swift
@@ -6,21 +6,48 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct TextFile: FileDocument {
+    static var readableContentTypes: [UTType] = [UTType.plainText]
+    
+    var text = ""
+    
+    init(initialText: String = "") {
+        text = initialText
+    }
+    init(configuration: ReadConfiguration) throws {
+        if let data = configuration.file.regularFileContents {
+            text = String(decoding: data, as: UTF8.self)
         }
-        .padding()
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = Data(text.utf8)
+        return FileWrapper(regularFileWithContents: data)
     }
 }
 
+
+
+struct ContentView: View {
+    
+    @Binding var document: TextFile
+    
+    var body: some View {
+        
+        TextEditor(text: $document.text)
+        
+    }
+}
+
+/**
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+*/
